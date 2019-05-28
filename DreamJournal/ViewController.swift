4ac04462a64fc446f8c0fceedd9ad2ec130b8a6c
Ania-chan <ania.jurago@gmail.com>
2019-05-28 11:35:10 +0900

    Initial Commit

---
+++ DreamJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DreamJournal
+//
+//  Created by Anna Nakayama on 2019/05/28.
+//  Copyright © 2019 Anna Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
